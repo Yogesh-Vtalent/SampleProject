545a4256afb5036daec59a564a6dd1dd42be3074
Yogesh-Vtalent <yogesh.vtalent@gmail.com>
2019-12-20 11:39:10 +0530

    Operatons Class Added Into The Project In Offline

---
+++ SampleProject/SampleProject/BussinessLayer/Operatons.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleProject.BussinessLayer
+{
+    public class Operatons
+    {
+        public int Addition(int A, int B)
+        {
+            return A + B;
+        }
+        public int Subtraction(int A, int B)
+        {
+            return A - B;
+        }
+        public int Multiplication()
+        {
+            int a = 5, b = 2, c;
+            return c = a * b;
+        }
+    }
+}
